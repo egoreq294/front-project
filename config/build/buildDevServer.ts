4969511f0ba3695ba7b1bff14dc3ecea62741b42
egor <egoreq@bk.ru>
2023-06-12 14:31:21 +0400

    add config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,9 @@
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+export const buildDevServer = (): DevServerConfiguration => {
+  return {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  };
+};
---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,32 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { RuleSetRule } from "webpack";
+import { BuildOptions } from "./types/config";
+
+export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: /\.module\./,
+            localIdentName: isDev
+              ? "[local]__[hash:base64:8]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, cssLoader];
+};
