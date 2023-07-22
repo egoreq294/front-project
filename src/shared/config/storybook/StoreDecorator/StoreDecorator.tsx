fec256e40130802fe43cc65f8c1a904293f51a8d
egor <egoreq@bk.ru>
2023-07-22 21:58:39 +0400

    fix snapshot

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -6,9 +6,11 @@ import {
 } from '../../../../app/providers/StorePovider';
 import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { loginReducer } from '@features/AuthByUsername/model/slice/loginSlice';
+import { profileReducer } from '@entities/Profile';
 
 const DEFAULT_ASYNC_REDUCERS: DeepPartial<ReducersMapObject<StateSchema>> = {
   loginForm: loginReducer,
+  profile: profileReducer,
 };
 
 export const StoreDecorator =
