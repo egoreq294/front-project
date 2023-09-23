d98b45b658891cefc3d0c133020cc3975dcd38e6
egor <egoreq@bk.ru>
2023-09-24 02:48:12 +0400

    fucking dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -23,6 +23,6 @@ export const buildWebpackConfig = (options: BuildOptions): Configuration => {
     },
     resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
-    devServer: buildDevServer(),
+    devServer: isDev ? buildDevServer() : undefined,
   };
 };
