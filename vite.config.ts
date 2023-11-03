ae0c1832d7f0f7c8f6d630c8764fcd0a4ec148fd
egor <egoreq@bk.ru>
2023-11-03 22:00:14 +0400

    feature: add vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [svgr({ include: '**/*.svg' }), react()],
+  resolve: {
+    alias: [
+      { find: '@app', replacement: '/src/app' },
+      { find: '@entities', replacement: '/src/entities' },
+      { find: '@features', replacement: '/src/features' },
+      { find: '@shared', replacement: '/src/shared' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@widgets', replacement: '/src/widgets' },
+    ],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API__: JSON.stringify('http://localhost:8000'),
+  },
+});
