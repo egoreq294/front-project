9d3ba40ff9a59cb7e46c066e94ab7e9433fe12ba
egor <egoreq@bk.ru>
2023-11-11 22:25:58 +0400

    fix: remove counter from mainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,12 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Counter } from '@entities/Counter';
 import { Page } from '@widgets/Page';
 
 const MainPage: FC = () => {
   const { t } = useTranslation('main');
 
-  return (
-    <Page>
-      {t('main-page')}
-      <Counter />
-    </Page>
-  );
+  return <Page>{t('main-page')}</Page>;
 };
 
 export default MainPage;
