e6e43bc75661fcceca4680df151b648b2ca26a4a
egor <egoreq@bk.ru>
2024-02-10 23:31:12 +0400

    fix: fix skeleton

--- client/src/entities/Article/ui/ArticleDetails/ArticleDetailsSkeleton.tsx
+++ client/src/entities/Article/ui/ArticleDetails/ArticleDetailsSkeleton.tsx
@@ -6,8 +6,8 @@ import { VStack } from '@shared/ui/Stack';
 export const ArticleDetailsSkeleton: FC = () => {
   return (
     <VStack gap="24" fullWidth>
-      <Skeleton width={300} height={32} />
-      <Skeleton width={600} height={24} />
+      <Skeleton width={'30%'} height={32} />
+      <Skeleton width={'50%'} height={24} />
       <Skeleton width={'100%'} height={200} />
       <Skeleton width={'100%'} height={200} />
     </VStack>
